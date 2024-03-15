9317e33dbf0c51b515f4467ce397c7b325cb6ea7
fillies <claas.fillies@donau-uni.ac.at>
2024-03-15 17:01:48 +0100

    start of task3

---
+++ claas/week2/task3.f90
@@ -0,0 +1,11 @@
+program statistics
+    implicit none
+    integer :: n, i
+    real :: number, total_sum, mean, sum_sq, std_dev
+    real, allocatable :: numbers(:)
+
+
+
+
+    
+    end program statistics
