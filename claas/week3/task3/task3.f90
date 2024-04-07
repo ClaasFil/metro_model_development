1f1bb94d3093c5cec73f91ceb6a5f66bdff65081
fillies <claas.fillies@donau-uni.ac.at>
2024-04-07 19:53:57 +0200

    task3 done and ready for handin

--- claas/week3/task3/task3.f90
+++ claas/week3/task3/task3.f90
@@ -10,16 +10,16 @@ program statistics
 
 
     !stet initial values
-    L = 20
-    nx = 20
-    total_time = 10000
+    L = 20  ! ength of the model domain
+    nx = 20 ! the number of grid points
+    total_time = 20 ! the total time of the simulation
 
 
     ! calculate the grid spacing
     h = L / (nx - 1)
     dt = 0.4 * h**2
 
-    !Initialize the temperature on the grid, either with a delta function
+    ! Initialize the temperature on the grid, with a delta function
     allocate(T_old(nx), T_new(nx), dT2(nx))
     dT2 = 0.0
     T_new = 0.0
@@ -27,11 +27,11 @@ program statistics
     T_old(nx/2) = 1.0
 
     ! set  the boundary condition T = 0 (for i = 1 and i = nx) independet of initialization
+    ! Since the deriverty is calculated only for the inner points, 
+    ! the boundary conditions are not updated 
     T_old(1) = 0.0
     T_old(nx) = 0.0
 
-    !print the initial temperature field
-    !print *, T_old
 
     ! Save results to a file
     open(1, file='task3_res.txt', status='replace')
@@ -48,15 +48,11 @@ program statistics
         !print *, T_new
         T_old = T_new
 
-        ! check if every element of T is close to 0
+        ! check anf break if every element of T is close to 0
         if (all(abs(T_new) < 0.01)) then
             exit
         end if
     end do
-
-
-
-
 
 
     !deallocate the temperature fields
