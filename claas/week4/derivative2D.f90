3cc97ae96f12a02dd9e9bad3b74e8528bdbe8bee
fillies <claas.fillies@donau-uni.ac.at>
2024-04-16 13:51:30 +0200

    everything but findein a done

--- claas/week4/derivative2D.f90
+++ claas/week4/derivative2D.f90
@@ -18,7 +18,39 @@ contains
     end subroutine print_matrix
 end module matrix_utilities
 
+module temperature_io
+    implicit none
+contains
+    subroutine write_temperature_to_csv(outfile, T)
+        implicit none
+        real, dimension(:,:), intent(in) :: T
+        character(len=*), intent(in)  :: outfile
+        integer :: io, i, j
 
+        ! Open the file for appending; create a new one if it doesn't exist
+        open(unit=20, file=trim(outfile), status='unknown', action='write', iostat=io, position='append')
+        if (io /= 0) then
+            print *, "Failed to open file:", trim(outfile)
+            return
+        end if
+
+
+        ! Write the matrix to the file
+        do i = 1, size(T, 1)
+            do j = 1, size(T, 2)
+                if (j == size(T, 2)) then
+                    write(20, '(F6.2)') T(i, j)  ! Last element in the row
+                else
+                    write(20, '(F6.2, A)', advance='no') T(i, j), ','  ! Elements with comma
+                end if
+            end do
+            write(20, *)  ! Newline for the next row
+        end do
+
+        ! Close the file
+        close(20)
+    end subroutine write_temperature_to_csv
+end module temperature_io
 
 module derivative2D
     implicit none
@@ -26,25 +58,20 @@ module derivative2D
 contains
     subroutine deriv2(y, h, second_derivative)
         use matrix_utilities
-        real, allocatable, intent(in) :: y(:,:)
+        real, intent(in) :: y(:,:)
         real, intent(in) :: h
-        real, allocatable, intent(out) :: second_derivative(:,:)
+        real, intent(out) :: second_derivative(:,:)
         integer :: n, m, i, j
         
         n = size(y, 1)
         m = size(y, 2)
         
-     
-        
-        
-
-        allocate(second_derivative(n,m))
 
         second_derivative = 0.0
+        
 
         do i = 2, n - 1
             do j = 2, m - 1
-  
                 second_derivative(i,j) = (y(i+1,j) + y(i-1,j) + y(i,j+1) - 4.0 * y(i,j) + y(i,j-1)) / h**2
             end do
         end do
@@ -62,7 +89,34 @@ end module derivative2D
 
 
 
+module norm_calculations
+    implicit none
+contains
+    subroutine calc_linfty_norm(matrix, norm_linfty)
+        ! Declare the subroutine arguments
+        real, intent(in) :: matrix(:,:)
+        real, intent(out) :: norm_linfty
+        integer :: n, m, i, j
+        real :: row_sum
 
+        ! Get the dimensions of the matrix
+        n = size(matrix, 1)
+        m = size(matrix, 2)
+
+        ! Initialize the L-infinity norm
+        norm_linfty = 0.0
+
+        ! Compute L-infinity norm (maximum row sum)
+        do i = 1, n
+            row_sum = 0.0
+            do j = 1, m
+                
+                row_sum = row_sum + abs(matrix(i, j))
+            end do
+            norm_linfty = max(norm_linfty, row_sum)
+        end do
+    end subroutine calc_linfty_norm
+end module norm_calculations
 
 
 
--- claas/week4/task1.f90
+++ claas/week4/task1.f90
@@ -1,10 +1,11 @@
 program statistics
     use derivative2D
     use matrix_utilities
+    use temperature_io
     implicit none
     INTEGER :: nx=10, ny=10, nsteps, temp=0, iter_count
-    REAL :: kappa=0.1, total_time=1.0, a=1.0, h, dt, i
-    real, allocatable :: T_old(:, :), T_new(:, :), dT2(:, :)    !temperature field (T) on the grid
+    REAL :: kappa=0.1, total_time=1.0, a=1.0, h, dt
+    real, allocatable :: T_old(:, :), T_new(:, :), dT2(:, :), T_rand(:,:)    !temperature field (T) on the grid
     ! Rean in the namelist
     NAMELIST /inputs/ nx, ny, kappa, total_time, a
 
@@ -23,19 +24,17 @@ program statistics
 
 
 
-    allocate(T_old(nx,ny), T_new(nx,ny), dT2(nx,ny))
+    allocate(T_old(nx,ny), T_new(nx,ny), dT2(nx,ny), T_rand(nx,ny))
     dT2 = 0.0
     T_new = 0.0
     T_old = 0.0
     T_old(int(nx/2), int(ny/2)) = 10.0   ! maybe inprofe finding the middle of the grid
+    call RANDOM_NUMBER(T_rand)
 
 
     
     !call print_matrix(T_old)
    
-
-    
-    
 
     ! set  the boundary condition T = 0 (for i = 1 and i = nx) independet of initialization
     ! Since the deriverty is calculated only for the inner points, 
@@ -44,14 +43,22 @@ program statistics
     T_old(nx,:) = 0.0
     T_old(:,1) = 0.0
     T_old(:,ny) = 0.0
+    T_rand(1,:) = 0.0
+    T_rand(nx,:) = 0.0
+    T_rand(:,1) = 0.0
+    T_rand(:,ny) = 0.0
+
+
 
     ! Save results to a file
-    open(1, file='task1_res.txt', status='replace')
-    write(1, *) 'nx'
-    write(1, *) nx
-    write(1, *) 'ny'
-    write(1, *) ny
-    write(1, *) T_old
+    open(1, file='task1_res.csv', status='replace')
+    
+    write(1, '(A, I0, A, I0)') 'nx,', nx, ',ny,', ny
+    close(1)
+
+
+    call write_temperature_to_csv('task1_res.csv', T_old)
+
 
     !time loop
 
@@ -59,28 +66,80 @@ program statistics
         
         !calculate the new temperature field
         call deriv2(T_old, h, dT2)
-        !call print_matrix(T_old + (dt* kappa * dT2))
         T_new = T_old + dt* kappa * dT2
-        call print_matrix(T_new)
-        write(1, *) T_new
+
+
+        
+        call write_temperature_to_csv('task1_res.csv', T_new)
+
         !print *, T_new
         T_old = T_new
 
         ! check anf break if every element of T is close to 0
-        if (all(abs(T_new) < 0.01)) then
+        if (all(abs(T_new) < 0.001)) then
             exit
         end if
 
-        if (temp > 10) then
+        if (temp > 200) then
             exit
         end if
         temp = temp +  1
 
         iter_count = iter_count + 1
     end do
+    print *, 'Number of iterations:', iter_count
+    
+    
+    ! Save results to a file
+    open(1, file='task1_res_rand.csv', status='replace')
+    
+    write(1, '(A, I0, A, I0)') 'nx,', nx, ',ny,', ny
+    close(1)
 
-    deallocate(T_old, T_new, dT2)
+
+    call write_temperature_to_csv('task1_res_rand.csv', T_rand)
+
+
+    !time loop
+    temp = 0
+    iter_count = 0
+    do while (iter_count < nsteps)
+        
+        !calculate the new temperature field
+        call deriv2(T_rand, h, dT2)
+        T_new = T_rand + dt* kappa * dT2
+
+
+        
+        call write_temperature_to_csv('task1_res_rand.csv', T_new)
+
+        !print *, T_new
+        T_rand = T_new
+
+        ! check anf break if every element of T is close to 0
+        if (all(abs(T_new) < 0.001)) then
+            exit
+        end if
+
+        if (temp > 200) then
+            exit
+        end if
+        temp = temp +  1
+
+        iter_count = iter_count + 1
+    end do
+    print *, 'Number of iterations:', iter_count
+    ! Close the file
+    
+
+    deallocate(T_old, T_new, dT2, T_rand)
+
+
+
+    
 
 
 
 end program statistics
+
+
