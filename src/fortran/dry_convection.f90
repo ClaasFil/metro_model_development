6ab2fa022fdf61f92d5933a07496e3b10d95b870
fillies <claas.fillies@donau-uni.ac.at>
2024-05-07 12:03:59 +0200

    trying to make one vcycle work but no success...

--- src/fortran/dry_convection.f90
+++ src/fortran/dry_convection.f90
@@ -20,7 +20,7 @@ program dry_convection
     real :: max_err                          ! maximum error
     real :: Ra                               ! Rayleigh number
     real(8) :: h                                ! grid spacing
-    real(8) :: alpha = 1.0                           ! relaxation parameter
+    real(8) :: alpha = 1.                         ! relaxation parameter
     REAL(8) :: res_rms                          ! root mean square residue 
     REAL(8) :: time = 0.0                     ! time. since h is change wee neet to track tiem
     character(len=100) :: T_ini_type        ! initial temperature profile type
@@ -199,7 +199,8 @@ program dry_convection
         ! print max element
         print *, 'max Tdx = ', MAXVAL(ABS(Tdx))
 
-
+        print *, 'h = ', h
+        print *, 'alpha = ', alpha
         ! Determine 𝜔 from Ra dT/dx using the Poisson solver (Equation 2)
         res_rms = Vcycle_2DPoisson(w, -Ra*Tdx ,h, alpha) ! TODO:: check if - is correct
         print *, 'res_rms verena of w = ', res_rms
