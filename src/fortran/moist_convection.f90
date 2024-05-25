37c6b78f488b2003c80e50e40aaf80ea691441b7
AltonV <verena.alton@ait.ac.at>
2024-05-25 10:19:30 +0200

    Model works somehow but we never have clouds

--- src/fortran/moist_convection.f90
+++ src/fortran/moist_convection.f90
@@ -26,6 +26,7 @@ program dry_convection
     real :: lambda = 0.2                    ! Latent heating
     real :: tau = 2.0                      ! Time scale for condensation
     real :: alpha = 1.0                       !  Exponential scaling factor for saturation specific humidity
+    real :: Sm = 1.33                       ! Ratio of diffusivities
     real(8) :: h                             ! grid spacing
     real(8) :: dt                            ! time step
     real(8) :: relax = 1.                         ! relaxation parameter
@@ -33,7 +34,10 @@ program dry_convection
     REAL(8) :: time = 0.0                     ! time. since h is change wee neet to track time
     REAL(8) :: f_norm                          ! norm used for convergence check of poisson solver
     character(len=100) :: T_ini_type = "cosine"        ! initial temperature profile type
-    character(len=100) :: outputfilename        ! output file name
+    character(len=100) :: outputfilename_T        ! output file name
+    character(len=100) :: outputfilename_q        ! output file name
+    character(len=100) :: outputfilename_C        ! output file name
+    character(len=100) :: outputfilename_psi        ! output file name
     real(8), allocatable:: T(:,:)               ! temperature profile
     real(8), allocatable:: T_new(:,:)           ! new temperature profile
     real(8), allocatable:: w(:,:)               ! vorticity
@@ -44,9 +48,16 @@ program dry_convection
     real(8), allocatable:: v(:,:)               ! y velocity
     real(8), allocatable:: d2T(:,:)             ! second derivative(x,y) of temperature
     real(8), allocatable:: d2w(:,:)             ! second derivative(x,y) of vorticity
+    real(8), allocatable:: d2q(:,:)             ! second derivative(x,y) of specific humidity
     real(8), allocatable:: vdT(:,:)             ! advection
     real(8), allocatable:: vdw(:,:)             ! advection
+    real(8), allocatable:: vdq(:,:)             ! advection
     real(8), allocatable:: RadTdx(:,:)          ! Ra * dT/dx
+    real(8), allocatable :: q(:,:)                        ! specific humidity
+    real(8), allocatable :: q_new(:,:)                    ! specific humidity
+    real(8), allocatable :: qs(:,:)                    ! saturation specific humidity
+    real(8), allocatable :: C(:,:)                    ! condensation
+    real(8), allocatable :: q_qs(:,:)                    ! heaviside function
     
 
     !read input parameters
@@ -70,7 +81,8 @@ program dry_convection
 
     ! Init Temperature arrays
     allocate(T(nx, ny), T_new(nx, ny), w(nx, ny), w_new(nx, ny), psi(nx, ny), Tdx(nx, ny), u(nx, ny), v(nx, ny))
-    allocate(vdT(nx, ny), vdw(nx,ny), d2T(nx, ny), d2w(nx, ny), RadTdx(nx, ny))
+    allocate(vdT(nx, ny), vdw(nx,ny), vdq(nx, ny), d2T(nx, ny), d2w(nx, ny), d2q(nx, ny), RadTdx(nx, ny))
+    allocate(q(nx, ny), qs(nx, ny), q_new(nx, ny), C(nx, ny), q_qs(nx, ny))
 
 
     ! Initialize arrays
@@ -84,7 +96,6 @@ program dry_convection
     v = 0.0
     d2T = 0.0
     d2w = 0.0
-
 
     h = 1.0 / (ny - 1)
 
@@ -108,15 +119,24 @@ program dry_convection
         stop
     end if
 
-    outputfilename = 'data/ex_9/T_cos.csv'
-    open(unit=10, file=trim(outputfilename), status='replace', action='write', iostat=io_error)
+    qs = exp(alpha * T)
+    q = 0.8 * qs
+
+    outputfilename_T = 'data/ex_9/T_cos.csv'
+    outputfilename_q = 'data/ex_9/q_cos.csv'
+    outputfilename_C = 'data/ex_9/C_cos.csv'
+    outputfilename_psi = 'data/ex_9/psi_cos.csv'
+    open(unit=10, file=trim(outputfilename_T), status='replace', action='write', iostat=io_error)
     if (io_error /= 0) then
         print *, 'Error opening file:', io_error
         stop
     endif
     close(10)
 
-    call write_to_csv_real8(outputfilename, T)
+    call write_to_csv_real8(outputfilename_T, T)
+    call write_to_csv_real8(outputfilename_q, q)
+    call write_to_csv_real8(outputfilename_C, C)
+    call write_to_csv_real8(outputfilename_psi, psi)
 
 
     k = 0
@@ -130,14 +150,12 @@ program dry_convection
         do while (res_rms/f_norm > max_err)
             res_rms = Vcycle_2DPoisson(psi, -w, h, relax) 
         end do
-        call boundaries_zero(psi)
 
         ! Compute the wind speeds 𝑢 and 𝑣 from 𝜓
         call dy(psi, h, u)
         call dx(-psi, h, v)
 
         ! Compute the time step from a_adv, a_diff and the maximum wind speed in the domain
-        !dt = MIN(a_diff*h**2, a_adv*h/MAX(MAXVAL(ABS(u)), MAXVAL(ABS(v))))  
         dt = MIN(a_diff*h**2/max(1.,Pr), a_adv*h/MAX(MAXVAL(ABS(u)), MAXVAL(ABS(v))))  
 
         ! Calc first derivertiv of temperature in x direction:
@@ -154,10 +172,19 @@ program dry_convection
         call FiniteDifference2D_real8(w, h, d2w)
         ! 𝑣⃑ * ∇𝜔 = advection
         call advection2D_real8(w, h, u, v, vdw)
-        
+
+        ! Compute the second derivative of specific humdity q
+        call FiniteDifference2D_real8(q, h, d2q)
+        ! 𝑣⃑ * ∇q = advection
+        call advection2D_real8(q, h, u, v, vdq)
+
         ! calc next T
-        T_new = T + dt * (d2T - vdT)
+        T_new = T + dt * (d2T - vdT - gamma * v) 
         T = T_new
+
+        ! Calc next q
+        q_new = q + dt * (Sm*d2q - vdq)
+        q = q_new
 
         ! calc next 𝜔
         w_new = w + dt * (Pr * d2w + Pr * RadTdx - vdw)
@@ -167,30 +194,61 @@ program dry_convection
         ! track time and iterations
         time = time + dt
         k = k + 1
+
+        !Diagnose condensation
+        q_qs = q - qs
+        C = (q - qs) / tau * heaviside(q_qs)
+        T_new = T + lambda * C
+        T = T_new
+        q_new = q - C
+        q = q_new
         
         ! Maxiter to protect from to long runtimes and make numerically instable behavior debugging easier
-        if (k >= 4000) then
+        if (k >= 3000) then
             exit
         end if
 
-        ! only plot a certain amount of matix to not hav to large files
+        ! only plot a certain amount of matrix to not have to large files
         !if (mod(k, 10) == 0) then
             !call write_to_csv_real8(outputfilename, T)
         !end if
     end do
-    call write_to_csv_real8(outputfilename, T)
+    call write_to_csv_real8(outputfilename_T, T)
+    call write_to_csv_real8(outputfilename_q, q)
+    call write_to_csv_real8(outputfilename_C, C)
+    call write_to_csv_real8(outputfilename_psi, psi)
 
     contains
 
         FUNCTION heaviside(x) RESULT(y)
-            REAL(8), INTENT(IN) :: x
-            REAL(8) :: y
-            if (x > 0.0) then
-                y = 1.0
-            else
-                y = 0.0
-            end if
+            REAL(8), INTENT(IN), allocatable :: x(:,:)
+            REAL(8), allocatable :: y(:,:)
+
+            nx = size(x, 1)
+            ny = size(x, 2)
+
+            allocate(y(nx, ny))
+
+            do CONCURRENT(i = 1:nx, j = 1:ny)
+                if (x(i, j) > 0.0) then
+                    y(i, j) = 1.0
+                else
+                    y(i, j) = 0.0
+                end if
+            end do
+            
         END FUNCTION heaviside
+
+        FUNCTION qsat(T) RESULT(q)
+            REAL(8), INTENT(IN), allocatable:: T(:,:)
+            REAL(8), allocatable :: q(:,:)
+
+            nx = size(T, 1)
+            ny = size(T, 2)
+            allocate(q(nx, ny))
+
+            q = EXP(alpha * T)
+        END FUNCTION qsat
 
 
 end program dry_convection
